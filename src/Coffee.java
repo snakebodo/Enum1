fe7044ba86ac84c2cfb8aa3dd84d212e046f7e4b
bogda <bogdanhalici1989@gmail.com>
2022-11-17 17:53:52 +0000

    1st commit

---
+++ src/Coffee.java
@@ -0,0 +1,10 @@
+public enum Coffee {
+    ;
+    private final int milk;
+    private final int water;
+
+    Coffee(int milk, int water) {
+        this.milk = milk;
+        this.water = water;
+    }
+}
